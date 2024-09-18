cccb524ae1f5a141d14f7d5b8a0c1510a02393bf
Arseny-Dernovich <arseni.dernovich@bk.ru>
2024-09-18 14:02:13 +0300

    Create Onegin.cpp

---
+++ Onegin.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <assert.h>
+#include <sys/stat.h>
+
+int Process_Onegin (void);
+
+
+
+int main ()
+{
+    return Process_Onegin ();
+}
+
+
